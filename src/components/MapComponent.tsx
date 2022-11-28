e4782cf8d59e17420159d76cbd4226c84e9f45ec
Gianx64 <gianpol1337@gmail.com>
2022-11-28 19:27:00 -0300

    Santiago en el mapa

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export const MapComponent = () => {
+    const [ubicacion, setUbicacion] = useState({
+        lat: -33.44,
+        lon: -70.65,
+    });
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} provider={'google'} showsUserLocation showsMyLocationButton />
+            <MapView style={styles.map}
+            provider={'google'}
+            showsUserLocation
+            showsMyLocationButton
+            initialRegion={{
+                latitude: ubicacion.lat,
+                longitude: ubicacion.lon,
+                latitudeDelta: 0.5,
+                longitudeDelta: 0.2,
+            }} />
         </View>
     );
 }
