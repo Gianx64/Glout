c39105e49df3c3a81a7cbeff20ecc27eb5ebd164
AleksMRCR <106041615+AleksMRCR@users.noreply.github.com>
2022-12-07 22:17:15 -0300

    v4

--- src/screens/ShowStoreScreen.tsx
+++ src/screens/ShowStoreScreen.tsx
@@ -9,11 +9,11 @@ export const ShowStoreScreen = ({ navigator, route }:any) => {
     return (
         <View style={styles.container}>
             <ScrollView>
-                <Text style={{fontSize: 40}}>Tienda: {route.params.store.name ?? 'No se sabe'}</Text>
-                <Text style={{fontSize: 30}}>Capacidad de delivery: {route.params.store.delivery ? 'Si' : 'No'}</Text>
-                <Text style={{fontSize: 30}}>Contacto: {route.params.store.contact ?? 'No se sabe'}</Text>
-                <Text style={{fontSize: 30}}>Descripción: {route.params.store.description ?? 'No se sabe'}</Text>
-                <Text style={{fontSize: 20}}>Likes: # / Dislikes: #</Text>
+                <Text style={{...styles.titulo}}>Tienda: {route.params.store.name ?? 'No se sabe'}</Text>
+                <Text style={{...styles.content}}>Capacidad de delivery: {route.params.store.delivery ? 'Si' : 'No'}</Text>
+                <Text style={{...styles.content}}>Contacto: {route.params.store.contact ?? 'No se sabe'}</Text>
+                <Text style={{...styles.content}}>Descripción: {route.params.store.description ?? 'No se sabe'}</Text>
+                <Text style={{...styles.coment}}>Likes: # / Dislikes: #</Text>
                 <BlackButton onPress = {() => alert('Boton presionado')} text='Guardar'/>
                 <LikeButton onPress = {() => alert('Boton presionado')} text='Me gusta' /> 
                 <DislikeButton onPress = {() => alert('Boton presionado')} text='No me gusta' />
@@ -32,6 +32,33 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
+    },
+    titulo:{
+        fontSize: 40,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: 10,
+
+    },
+    content:{
+        fontSize: 20,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: 10,
+
+    },
+    coment:{
+        fontSize: 15,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: 10,
+
     },
 
 });
