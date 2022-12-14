655285a6cbe6d20936616b84801f0822afbca5f5
Gianx64 <gianpol1337@gmail.com>
2022-12-14 15:33:14 -0300

    MapComponent para MapScreen

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -5,48 +5,7 @@ import { MapComponent } from '../components/mapComponents/MapComponent';
 export const MapScreen = () => {
     return (
         <View style={styles.container}>
-            <MapView style={styles.map}
-            provider={'google'}
-            showsUserLocation
-            showsMyLocationButton
-            initialRegion={{
-                latitude: ubicacion.latitude,
-                longitude: ubicacion.longitude,
-                latitudeDelta: 0.5,
-                longitudeDelta: 0.2,
-            }}>
-                <Marker draggable
-                    coordinate={ubicacion}
-                    onDragEnd={(direction) => setUbicacion(direction.nativeEvent.coordinate)}
-                >
-                    <Callout onPress= {() => {navigation.navigate('SaveStoreScreen', { ubicacion })}}>
-                        <TouchableHighlight>
-                            <View>
-                                <Text>Ingresar tienda</Text>
-                            </View>
-                        </TouchableHighlight>
-                    </Callout>
-                </Marker>
-                {data.map((store, i) => {
-                    return (
-                        <Marker
-                            pinColor='blue'
-                            coordinate={store.coords}
-                        >
-                            <Callout onPress= {() => {navigation.navigate('ShowStoreScreen', { store })}}>
-                                <TouchableHighlight>
-                                    <View>
-                                        <Text>Ver tienda {store.description}</Text>
-                                    </View>
-                                </TouchableHighlight>
-                            </Callout>
-                        </Marker>
-                    ) 
-                })}
-			</MapView>
-            <FAB
-                style={{mapstyle}}
-                icon="plus" />
+            <MapComponent />
         </View>
     )
 }
