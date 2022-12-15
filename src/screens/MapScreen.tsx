a99f2e4b27e3c328e85a67630a41869294350dbc
Gianx64 <GianPol1337@Gmail.com>
2022-12-15 16:44:09 -0300

    Botón explicativo "ayuda"

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { MapComponent } from '../components/mapComponents/MapComponent';
+import { Callout } from 'react-native-maps';
+import { FAB } from 'react-native-paper';
 
 export const MapScreen = () => {
     return (
         <View style={styles.container}>
             <MapComponent />
+            <Callout style={styles.buttonCallout}>
+                <TouchableOpacity
+                    style={[styles.touchable]}
+                    onPress={() => alert('El pin rojo se puede arrastrar manteniéndolo presionado y su función es añadir una tienda, para hacer esto, toque el pin rojo cuando lo posicione donde desea, luego, toque el texto "Ingresar tienda", esto le llevará al formulario para ingresar los datos de la tienda.\nLos pines azules son ubicaciones de tiendas. Para ver los detalles de la tienda, toque el pin que desee y luego el texto "Tienda: ...".')}
+                >
+                    <Text style={styles.touchableText}>Ayuda</Text>
+                </TouchableOpacity>
+            </Callout>
         </View>
     )
 }
@@ -16,5 +26,35 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         width: '100%',
+    },
+    //fab
+    flex: {
+        flex: 1
+    },
+    fab: {
+        position: "absolute",
+        right: 50,
+        bottom: 50,
+        margin: 20
+    },
+    //fab
+    buttonCallout: {
+        flex: 1,
+        flexDirection:'row',
+        position:'absolute',
+        bottom:10,
+        alignSelf: "center",
+        justifyContent: "space-between",
+        backgroundColor: "transparent",
+        borderWidth: 0.5,
+        borderRadius: 100
+    },
+    touchable: {
+        backgroundColor: "lightblue",
+        padding: 10,
+        margin: 10
+    },
+    touchableText: {
+        fontSize: 16
     },
 });
