b07fc7d5d52e78dd8b64eec7e1d5a2f6ca58b0bb
Gianx64 <GianPol1337@Gmail.com>
2022-12-15 16:40:56 -0300

    Borrar drawer

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -10,52 +8,28 @@ import HomeScreen from './src/screens/HomeScreen';
 import SaveStoreScreen from './src/screens/SaveStoreScreen';
 import UserScreen from './src/screens/UserScreen';
 import ShowStoreScreen from './src/screens/ShowStoreScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
 	return (
-		//<NavigationContainer>{
-			<Stack.Navigator initialRouteName="SignIn Screen">
-				<Stack.Screen name="Home Screen" component={HomeScreen} />
-				<Stack.Screen name="User Screen" component={UserScreen} />
-				<Stack.Screen name="SignUp Screen" component={SignUpScreen} />
-				<Stack.Screen name="SignIn Screen" component={SignInScreen} />
-				<Stack.Screen name="Map Screen" component={MapScreen} />
-				<Stack.Screen name="SaveStoreScreen" component={SaveStoreScreen} />
-				<Stack.Screen name="ShowStoreScreen" component={ShowStoreScreen} />
-			</Stack.Navigator>
-		//}</NavigationContainer>
+		<Stack.Navigator initialRouteName="SignIn Screen">
+			<Stack.Screen name="Home Screen" component={HomeScreen} />
+			<Stack.Screen name="User Screen" component={UserScreen} />
+			<Stack.Screen name="SignUp Screen" component={SignUpScreen} />
+			<Stack.Screen name="SignIn Screen" component={SignInScreen} />
+			<Stack.Screen name="Map Screen" component={MapScreen} />
+			<Stack.Screen name="SaveStoreScreen" component={SaveStoreScreen} />
+			<Stack.Screen name="ShowStoreScreen" component={ShowStoreScreen} />
+		</Stack.Navigator>
 	)
 }
-/*
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-	return (
-		<Drawer.Navigator useLegacyImplementation={true} initialRouteName="MapScreen">
-			<Drawer.Screen name="Home Screen" component={HomeScreen} />
-			<Drawer.Screen name="SignUp Screen" component={SignUpScreen} />
-			<Drawer.Screen name="SignIn Screen" component={SignInScreen} />
-			<Drawer.Screen name="Map Screen" component={MapScreen} />
-		</Drawer.Navigator>
-	)
-}*/
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			{/* <MyDrawer /> */}
 			<MyStack />
 		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
