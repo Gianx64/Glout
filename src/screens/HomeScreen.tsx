3e7cafd2e538746539222368c7671fff94d81060
Diegogl09 <diego.gamonall@utem.cl>
2022-12-15 00:22:32 -0300

    Logo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { Button, StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, View, Image } from 'react-native'
 import { ReadDataComponent } from '../components/databaseComponents/ReadDataComponent';
 
 
@@ -8,6 +8,10 @@ export const HomeScreen = () => {
 	const navigation = useNavigation();
     return (
         <View style={styles.container}>
+            <Image
+             source={require('../../assets/icon.png')}
+             style={{ width: 200, height: 200 }}
+            />
             {/* <WriteDataComponent />*/}
             <ReadDataComponent />
             <Button title={'Ir a perfil'} onPress={() => {navigation.navigate('User Screen')}} />
