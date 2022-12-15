30231b72d24613cb5a211b1a677d83e1b5627edf
Diegogl09 <diego.gamonall@utem.cl>
2022-12-15 04:51:07 -0300

    home con diseño

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,32 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { Button, StyleSheet, View, Image } from 'react-native'
+import { Button, StyleSheet, View, Image, Text } from 'react-native'
 import { ReadDataComponent } from '../components/databaseComponents/ReadDataComponent';
 
 
 export const HomeScreen = () => {
 	const navigation = useNavigation();
     return (
-        <View style={styles.container}>
-            <Image
-             source={require('../../assets/icon.png')}
-             style={{ width: 200, height: 200 }}
-            />
-            {/* <WriteDataComponent />*/}
-            <ReadDataComponent />
-            <Button title={'Ir a perfil'} onPress={() => {navigation.navigate('User Screen')}} />
-            <Button title={'Ir al mapa'} onPress={() => {navigation.navigate('Map Screen')}} />
-        </View>
+    <>
+         <View style={styles.container}>
+             <Image
+                source={require('../../assets/icon.png')}
+                style={{ width: 250, height: 250 }}
+             />
+         </View>
+
+         <View style={styles.data}>
+             {/* <WriteDataComponent />*/}
+             <ReadDataComponent />
+             <Button title={'Ir a perfil'} onPress={() => {navigation.navigate('User Screen')}} />
+             <Button title={'Ir al mapa'} onPress={() => {navigation.navigate('Map Screen')}} />
+         </View>
+
+         <View style={styles.text}>
+            <Text>Tu Aplicación celíaca favorita </Text>  
+
+         </View>
+     </>
     )
 }
 
@@ -24,6 +34,25 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#d6f9f8',
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+
+    data: {
+        backgroundColor: '#d6f9f8',
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+
+    text: {
+        backgroundColor: '#d6f9f8',
         flex: 1,
         display: 'flex',
         alignItems: 'center',
